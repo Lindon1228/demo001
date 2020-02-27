92013c3cd484391d88be8dcd54e59641a12644a0
Lindon <532215423@qq.com>
2020-02-27 14:39:23 +0800

    添加首页模型

---
+++ XIBZSYDemo/ShowYeModel.h
@@ -0,0 +1,17 @@
+//
+//  ShowYeModel.h
+//  XIBZSYDemo
+//
+//  Created by Lindon on 2020/2/27.
+//  Copyright © 2020 Lindon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ShowYeModel : YKBaseModel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XIBZSYDemo/ShowYeModel.m
@@ -0,0 +1,13 @@
+//
+//  ShowYeModel.m
+//  XIBZSYDemo
+//
+//  Created by Lindon on 2020/2/27.
+//  Copyright © 2020 Lindon. All rights reserved.
+//
+
+#import "ShowYeModel.h"
+
+@implementation ShowYeModel
+
+@end
