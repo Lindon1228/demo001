e57a8edc3db8c1b6c5635f4fea64610c775ea47b
Lindon <532215423@qq.com>
2020-02-27 14:31:42 +0800

    创建测试模型

---
+++ XIBZSYDemo/TestModel.h
@@ -0,0 +1,17 @@
+//
+//  TestModel.h
+//  XIBZSYDemo
+//
+//  Created by Lindon on 2020/2/27.
+//  Copyright © 2020 Lindon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestModel : YKBaseModel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XIBZSYDemo/TestModel.m
@@ -0,0 +1,13 @@
+//
+//  TestModel.m
+//  XIBZSYDemo
+//
+//  Created by Lindon on 2020/2/27.
+//  Copyright © 2020 Lindon. All rights reserved.
+//
+
+#import "TestModel.h"
+
+@implementation TestModel
+
+@end
